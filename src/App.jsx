7b35e314ca49746fd795314403e8b86110f90dc2
maruwebdev <shubhanshumaru2@gmail.com>
2025-12-19 12:23:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex-center h-screen'>
+      <h1 className='text-3xl text-indigo-300'>Shubhanshu</h1>
+    </div>
+  )
+}
+
+export default App
